8b3a4c51fb1b712f6112581899a00a3433e4ee93
Ji ZHANG <zhangji87@gmail.com>
2018-09-07 15:58:57 +0800

    use rc-form

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -4,44 +4,50 @@ import { RouteComponentProps } from 'react-router'
 import { inject, observer } from 'mobx-react'
 import { WhiteSpace, List, InputItem, Button, WingBlank, Toast } from 'antd-mobile'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { createForm, createFormField } from 'rc-form'
 import LoginStore from '../stores/login'
 import './Login.less'
 import logo from '../assets/logo.svg'
 
 interface Props extends RouteComponentProps<any> {
   loginStore?: LoginStore,
+  form: any,
 }
 
-@inject('loginStore')
-@observer
-export default class Login extends React.Component<Props> {
+class Login extends React.Component<Props> {
   public componentDidMount() {
     this.props.loginStore!.resetForm()
   }
 
   private handleLogin = () => {
-    this.props.loginStore!.login().then(() => {
-      let { nickname } = this.props.loginStore!.currentUser
-      Toast.success(`Welcome, ${nickname}!`, 1, () => {
-        this.props.history.push('/')
-      })
-    }, _.noop)
-  }
-
-  private handleChangeUsername = (value: string) => {
-    this.props.loginStore!.updateForm({
-      username: value,
+    this.props.form.validateFields((error: any) => {
+      if (!error) {
+        this.props.loginStore!.login().then(() => {
+          let { nickname } = this.props.loginStore!.currentUser
+          Toast.success(`Welcome, ${nickname}!`, 1, () => {
+            this.props.history.push('/')
+          })
+        }, _.noop)
+      }
     })
   }
 
-  private handleChangePassword = (value: string) => {
-    this.props.loginStore!.updateForm({
-      password: value,
-    })
+  private getFieldProps(name: string, fieldOptions: any) {
+    let { form } = this.props.loginStore!
+    let { getFieldProps } = this.props.form
+    let props = getFieldProps(name, fieldOptions)
+    return {
+      ...props,
+      error: !!form[name].errors,
+      onErrorClick: () => {
+        let message = form[name].errors[0].message
+        Toast.info(message)
+      }
+    }
   }
 
   public render() {
-    let { form } = this.props.loginStore!
+    let { loggingIn } = this.props.loginStore!
     return (
       <div className="page-login">
         <div className="app-title">
@@ -54,8 +60,11 @@ export default class Login extends React.Component<Props> {
         <List>
           <InputItem
             placeholder="Username"
-            value={form.username}
-            onChange={this.handleChangeUsername}
+            {...this.getFieldProps('username', {
+              rules: [
+                { required: true }
+              ],
+            })}
           >
             <div className="label-title">
               <FontAwesomeIcon icon="user" />
@@ -64,8 +73,11 @@ export default class Login extends React.Component<Props> {
           <InputItem
             placeholder="Password"
             type="password"
-            value={form.password}
-            onChange={this.handleChangePassword}
+            {...this.getFieldProps('password', {
+              rules: [
+                { required: true },
+              ],
+            })}
           >
             <div className="label-title">
               <FontAwesomeIcon icon="key" />
@@ -76,9 +88,28 @@ export default class Login extends React.Component<Props> {
         <WhiteSpace size="lg" />
 
         <WingBlank>
-          <Button type="primary" onClick={this.handleLogin}>Login</Button>
+          <Button
+            type="primary"
+            onClick={this.handleLogin}
+            loading={loggingIn}
+          >
+            Login
+          </Button>
         </WingBlank>
       </div>
     )
   }
 }
+
+export default inject('loginStore')(observer(createForm({
+  mapPropsToFields(props: Props) {
+    let { form } = props.loginStore!
+    return {
+      username: createFormField(form.username.value),
+      password: createFormField(form.password.value),
+    }
+  },
+  onFieldsChange(props: Props, fields: any) {
+    props.loginStore!.updateForm(fields)
+  },
+})(Login)))
--- src/stores/login.tsx
+++ src/stores/login.tsx
@@ -1,40 +1,63 @@
-import * as _ from 'lodash'
-import * as qs from 'qs'
+import  _ from 'lodash'
 import * as request from '../services/request'
 import RootStore from './root'
 import { observable, flow, action } from 'mobx'
 
+interface ValidateError {
+  field: string,
+  message: string,
+}
+
+interface FormItem {
+  value: string,
+  errors?: ValidateError[],
+}
+
+interface LoginForm {
+  username: FormItem,
+  password: FormItem,
+}
+
+function defaultForm(): LoginForm {
+  return {
+    username: { value: '' },
+    password: { value: '' },
+  }
+}
+
 export default class LoginStore {
   public readonly rootStore: RootStore
 
-  @observable public form = {
-    username: '',
-    password: '',
-  }
+  @observable public form: LoginForm = defaultForm()
 
   @observable public currentUser = {
     id: 0,
     nickname: '',
   }
 
+  @observable public loggingIn = false
   @observable public loggingOut = false
 
   constructor(rootStore: RootStore) {
     this.rootStore = rootStore
   }
 
-  @action public updateForm(form: object) {
+  @action public updateForm(form: any) {
     _.assign(this.form, form)
   }
 
   @action public resetForm() {
-    this.form.username = ''
-    this.form.password = ''
+    this.form = defaultForm()
   }
 
   public login = flow(function * (this: LoginStore) {
-    let payload = yield request.post('/api/login', this.form)
-    _.assign(this.currentUser, payload)
+    try {
+      let form = _.mapValues(this.form, 'value')
+      let payload = yield request.post('/api/login', form)
+      _.assign(this.currentUser, payload)
+    } finally {
+      this.loggingIn = false
+    }
   })
 
   public logout = flow(function * (this: LoginStore) {
