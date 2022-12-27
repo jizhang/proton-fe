985aec3b5cdf51966cbb8e6cace997be97de398d
Ji ZHANG <zhangji87@gmail.com>
2022-12-27 13:56:35 +0800

    Update dashboard

--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { RouteComponentProps } from 'react-router'
-import { NavBar, Icon, WhiteSpace } from 'antd-mobile-v2'
+import { NavBar } from 'antd-mobile'
 import Primary from '../components/dashboard/Primary'
 import Realtime from '../components/dashboard/Realtime'
 import UserSource from '../components/dashboard/UserSource'
@@ -12,43 +12,38 @@ import './Dashboard.less'
 
 interface Props extends RouteComponentProps<any> {}
 
-export default class Dashboard extends React.Component<Props> {
-  private gotoHome = () => {
-    this.props.history.push('/')
+export default (props: Props) => {
+  const cards = [
+    { name: 'primary', component: <Primary /> },
+    { name: 'realtime', component: <Realtime /> },
+    { name: 'user_source', component: <UserSource /> },
+    { name: 'world_geo', component: <SourceRegion /> },
+    { name: 'visit_time', component: <VisitTime /> },
+    { name: 'user_device', component: <UserDevice /> },
+    { name: 'user_retention', component: <UserRetention /> },
+  ]
+
+  const gotoHome = () => {
+    props.history.push('/')
   }
 
-  public render() {
-    let cards = [
-      { name: 'primary', component: <Primary /> },
-      { name: 'realtime', component: <Realtime /> },
-      { name: 'user_source', component: <UserSource /> },
-      { name: 'world_geo', component: <SourceRegion /> },
-      { name: 'visit_time', component: <VisitTime /> },
-      { name: 'user_device', component: <UserDevice /> },
-      { name: 'user_retention', component: <UserRetention /> },
-    ]
+  return (
+    <div className="page-dashboard">
+      <NavBar
+        className="nav"
+        onBack={gotoHome}
+      >
+        Dashboard
+      </NavBar>
 
-    return (
-      <div className="page-dashboard">
-        <NavBar
-          mode="dark"
-          icon={<Icon type="left" />}
-          onLeftClick={this.gotoHome}
-          rightContent={<Icon type="ellipsis" />}
-          className="nav"
-        >
-          Dashboard
-        </NavBar>
+      <div className="divider" style={{ marginTop: '45px' }}></div>
 
-        <WhiteSpace style={{ marginTop: 45 }} />
-
-        {cards.map(({ name, component }) => (
-          <React.Fragment key={name}>
-            {component}
-            <WhiteSpace />
-          </React.Fragment>
-        ))}
-      </div>
-    )
-  }
+      {cards.map(({ name, component }) => (
+        <React.Fragment key={name}>
+          {component}
+          <div className="divider"></div>
+        </React.Fragment>
+      ))}
+    </div>
+  )
 }
