fcc729c02f5468a381f39c61668e8daea8a1ecf0
Jerry <zhangji87@gmail.com>
2022-12-28 14:26:25 +0800

    Update dashboard store

--- src/stores/dashboard.tsx
+++ src/stores/dashboard.tsx
@@ -1,36 +1,37 @@
-import * as _ from 'lodash'
-import { observable, flow } from 'mobx'
+import _ from 'lodash'
+import { makeAutoObservable } from 'mobx'
 import RootStore from './root'
 
 export default class DashboardStore {
-  public readonly rootStore: RootStore
+  rootStore
 
-  @observable public realtime = {
+  realtime = {
     count: 0,
     previousCount: 0,
     minutes: [] as number[],
   }
 
-  @observable public userRetention = {
+  userRetention = {
     data: [] as any[],
   }
 
   constructor(rootStore: RootStore) {
+    makeAutoObservable(this, { rootStore: false })
     this.rootStore = rootStore
   }
 
-  public fetchActiveUser = flow(function * (this: DashboardStore) {
+  *fetchActiveUser(): any {
     let response = yield fetch('/api/dashboard/activeUser')
     let { payload } = yield response.json()
     _.assign(this.realtime, {
       previousCount: this.realtime.count,
       ...payload,
     })
-  })
+  }
 
-  public fetchUserRetention = flow(function * (this: DashboardStore) {
+  *fetchUserRetention(): any {
     let response = yield fetch('/api/dashboard/userRetention')
     let { payload } = yield response.json()
     this.userRetention.data = payload.retention
-  })
+  }
 }
