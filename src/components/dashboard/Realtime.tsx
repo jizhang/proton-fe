78f04d9178673a648f109e02d7bb344894bf3b53
Jerry <zhangji87@gmail.com>
2022-12-28 15:18:36 +0800

    Switch to mobx-react-lite

--- src/components/dashboard/Realtime.tsx
+++ src/components/dashboard/Realtime.tsx
@@ -1,63 +1,55 @@
-import * as React from 'react'
-import { observer, inject } from 'mobx-react'
-import * as _ from 'lodash'
+import React, { useEffect, useContext } from 'react'
+import { observer } from 'mobx-react-lite'
+import _ from 'lodash'
 import { Card } from 'antd-mobile'
 import { Chart, Geom } from 'bizcharts'
 import CountUp from 'react-countup'
-import DashboardStore from '../../stores/dashboard'
+import { RootStoreContext } from '../../stores'
 import './Realtime.less'
 
-interface Props {
-  dashboardStore?: DashboardStore,
-}
+export default observer(() => {
+  let activeUserHandler: number
 
-@inject('dashboardStore')
-@observer
-export default class Realtime extends React.Component<Props> {
-  private activeUserHandler: number
-
-  public componentDidMount() {
-    this.getActiveUser()
-    this.activeUserHandler = window.setInterval(() => {
-      this.getActiveUser()
+  useEffect(() => {
+    getActiveUser()
+    activeUserHandler = window.setInterval(() => {
+      getActiveUser()
     }, 5000)
-  }
 
-  public componentWillUnmount() {
-    if (this.activeUserHandler) {
-      window.clearInterval(this.activeUserHandler)
+    return () => {
+      window.clearInterval(activeUserHandler)
     }
+  }, [])
+
+  const { dashboardStore } = useContext(RootStoreContext)
+
+  function getActiveUser() {
+    dashboardStore.fetchActiveUser()
   }
 
-  private getActiveUser = () => {
-    this.props.dashboardStore!.fetchActiveUser()
-  }
+  const { realtime } = dashboardStore
+  const data = _.map(realtime.minutes, (value, index) => {
+    return { x: index, y: value }
+  })
 
-  public render() {
-    let { realtime } = this.props.dashboardStore!
-    let data = _.map(realtime.minutes, (value, index) => {
-      return { x: index, y: value }
-    })
-
-    return (
-      <Card
-        className="dashboard-realtime"
-        title="Real time"
-        extra="Last 5 minutes"
-        style={{ borderRadius: 0 }}
-      >
-        <CountUp
-          className="value-lg"
-          start={realtime.previousCount}
-          end={realtime.count}
-          duration={2.75}
-          separator=","
-        />
-        <div className="uv-title">Unique views per minute</div>
-        <Chart autoFit height={100} data={data} padding={[0, 8, 0, 8]}>
-          <Geom type="interval" position="x*y" size={8} />
-        </Chart>
-      </Card>
-    )
-  }
-}
+  return (
+    <Card
+      className="dashboard-realtime"
+      title="Real time"
+      extra="Last 5 minutes"
+      style={{ borderRadius: 0 }}
+    >
+      <CountUp
+        className="value-lg"
+        start={realtime.previousCount}
+        end={realtime.count}
+        duration={2.75}
+        separator=","
+      />
+      <div className="uv-title">Unique views per minute</div>
+      <Chart autoFit height={100} data={data} padding={[0, 8, 0, 8]}>
+        <Geom type="interval" position="x*y" size={8} />
+      </Chart>
+    </Card>
+  )
+})
