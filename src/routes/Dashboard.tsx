50792d008e153ace72a65834d39399d26d441332
Jerry <zhangji87@gmail.com>
2023-01-03 10:21:17 +0800

    Update dashboard

--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -8,7 +8,7 @@ import SourceRegion from '../components/dashboard/SourceRegion'
 import VisitTime from '../components/dashboard/VisitTime'
 import UserDevice from '../components/dashboard/UserDevice'
 import UserRetention from '../components/dashboard/UserRetention'
-import './Dashboard.less'
+import * as styles from './Dashboard.module.less'
 
 export default () => {
   const navigate = useNavigate()
@@ -28,17 +28,17 @@ export default () => {
   }
 
   return (
-    <div className="page-dashboard">
-      <NavBar className="nav" onBack={gotoHome}>
+    <div className={styles.dashboard}>
+      <NavBar className={styles.nav} onBack={gotoHome}>
         Dashboard
       </NavBar>
 
-      <div className="divider" style={{ marginTop: '45px' }}></div>
+      <div className={styles.divider} style={{ marginTop: '45px' }}></div>
 
       {cards.map(({ name, component }) => (
         <React.Fragment key={name}>
           {component}
-          <div className="divider"></div>
+          <div className={styles.divider}></div>
         </React.Fragment>
       ))}
     </div>
