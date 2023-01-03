f46529639803fb74ea0cb8e901ba4a9c5633d416
Jerry <zhangji87@gmail.com>
2023-01-03 10:38:01 +0800

    Update realtime

--- src/components/dashboard/Realtime.tsx
+++ src/components/dashboard/Realtime.tsx
@@ -4,8 +4,8 @@ import _ from 'lodash'
 import { Card } from 'antd-mobile'
 import { Chart, Geom } from 'bizcharts'
 import CountUp from 'react-countup'
-import { RootStoreContext } from '../../stores'
-import './Realtime.less'
+import { RootStoreContext } from '~/src/stores'
+import * as styles from './Realtime.module.less'
 
 export default observer(() => {
   let activeUserHandler: number
@@ -34,19 +34,18 @@ export default observer(() => {
 
   return (
     <Card
-      className="dashboard-realtime"
       title="Real time"
       extra="Last 5 minutes"
       style={{ borderRadius: 0 }}
     >
       <CountUp
-        className="value-lg"
+        className={styles.value}
         start={realtime.previousCount}
         end={realtime.count}
         duration={2.75}
         separator=","
       />
-      <div className="uv-title">Unique views per minute</div>
+      <div className={styles.title}>Unique views per minute</div>
       <Chart autoFit height={100} data={data} padding={[0, 8, 0, 8]}>
         <Geom type="interval" position="x*y" size={8} />
       </Chart>
