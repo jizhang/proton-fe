d5d4078cc6d342a878e58acb0a3c890e07b8945c
Jerry <zhangji87@gmail.com>
2023-03-24 10:34:44 +0800

    Mock api

--- src/components/realtime/Acquisition.tsx
+++ src/components/realtime/Acquisition.tsx
@@ -1,15 +1,8 @@
 import React, { useState, useEffect } from 'react'
-import _ from 'lodash'
 import * as service from '~/src/services/realtime-overview'
 import ListPanel from './ListPanel'
 
 export default () => {
-  const sampleData = [
-    { key: 'google', value: 1777, percent: 0.9052 },
-    { key: '(direct)', value: 173, percent: 0.0881 },
-    { key: 'pbb.lviv.ua', value: 13, percent: 0.0066 },
-  ]
-
   const [dimensionName, setDimensionName] = useState('First user source')
   const [topData, setTopData] = useState<service.TopData>({
     key: '-',
@@ -19,11 +12,9 @@ export default () => {
   })
 
   useEffect(() => {
-    setTopData(
-      _.assign(_.clone(sampleData[0]), {
-        chart: _.times(30, () => _.random(1000, 2000) * _.random(0, 5)),
-      })
-    )
+    service.getUserAcquisitionTop(dimensionName).then((payload) => {
+      setTopData(payload)
+    })
   }, [dimensionName])
 
   const [page, setPage] = useState(1)
@@ -33,9 +24,8 @@ export default () => {
   })
 
   useEffect(() => {
-    setListData({
-      data: sampleData,
-      total: sampleData.length,
+    service.getUserAcquisitionList(dimensionName, page).then((payload) => {
+      setListData(payload)
     })
   }, [dimensionName, page])
 
