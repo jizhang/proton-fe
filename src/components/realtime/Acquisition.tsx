14e975184d405233b685fe9709b23154dd3f0f16
Jerry <zhangji87@gmail.com>
2023-03-24 09:53:34 +0800

    Switch props

--- src/components/realtime/Acquisition.tsx
+++ src/components/realtime/Acquisition.tsx
@@ -1,28 +1,54 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import _ from 'lodash'
+import * as service from '~/src/services/realtime-overview'
 import ListPanel from './ListPanel'
 
 export default () => {
-  const [dimensionName, setDimensionName] = useState('First user source')
-
-  const chartData = _.times(30, () => _.random(1000, 2000) * _.random(0, 5))
-
-  const listData = [
+  const sampleData = [
     { key: 'google', value: 1777, percent: 0.9052 },
     { key: '(direct)', value: 173, percent: 0.0881 },
     { key: 'pbb.lviv.ua', value: 13, percent: 0.0066 },
   ]
+
+  const [dimensionName, setDimensionName] = useState('First user source')
+  const [topData, setTopData] = useState<service.TopData>({
+    key: '-',
+    value: 0,
+    percent: 0,
+    chart: [],
+  })
+
+  useEffect(() => {
+    setTopData(
+      _.assign(_.clone(sampleData[0]), {
+        chart: _.times(30, () => _.random(1000, 2000) * _.random(0, 5)),
+      })
+    )
+  }, [dimensionName])
+
+  const [page, setPage] = useState(1)
+  const [listData, setListData] = useState<service.ListData>({
+    data: [],
+    total: 0,
+  })
+
+  useEffect(() => {
+    setListData({
+      data: sampleData,
+      total: sampleData.length,
+    })
+  }, [dimensionName, page])
 
   return (
     <ListPanel
       measureName="Users"
       dimensionName={dimensionName}
       dimensionOptions={['First user source', 'First user medium', 'First user source / medium']}
-      chartData={chartData}
-      listData={listData}
-      onChangeDimensionName={(value) => {
-        setDimensionName(value)
-      }}
+      onChangeDimensionName={setDimensionName}
+      topData={topData}
+      listDataV2={listData}
+      page={page}
+      onChangePage={setPage}
     />
   )
 }
--- src/components/realtime/PageTitle.tsx
+++ src/components/realtime/PageTitle.tsx
@@ -3,8 +3,6 @@ import * as service from '~/src/services/realtime-overview'
 import ListPanel from './ListPanel'
 
 export default () => {
-  const [page, setPage] = useState(1)
-
   const [topData, setTopData] = useState<service.TopData>({
     key: '-',
     value: 0,
@@ -12,16 +10,17 @@ export default () => {
     chart: [],
   })
 
-  const [listData, setListData] = useState<service.ListData>({
-    data: [],
-    total: 0,
-  })
-
   useEffect(() => {
     service.getViewsByPageTitleTop().then((payload) => {
       setTopData(payload)
     })
   }, [])
+
+  const [page, setPage] = useState(1)
+  const [listData, setListData] = useState<service.ListData>({
+    data: [],
+    total: 0,
+  })
 
   useEffect(() => {
     service.getViewsByPageTitleList(page).then((payload) => {
